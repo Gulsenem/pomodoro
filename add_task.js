5e33feb0b8239f23a6df2871d3ce5591217873ec
Gulsenem <60161518+Gulsenem@users.noreply.github.com>
2022-03-17 14:55:04 +0100

    add task

--- add_task.js
+++ add_task.js
@@ -1,4 +1,36 @@
+let txarea = document.getElementById("addTask_txarea");
+
+
+
+
+
 function add_task()
 {
     document.getElementById("add_task_box").style.display="block";
+    
+}
+
+function cancel_task()
+{
+    document.getElementById("add_task_box").style.display="none";
+    txarea.value= "";
+}
+
+function save_task()
+{
+    document.getElementById("add_task_box").style.display="none";
+
+    let createDiv = document.createElement("div");
+    createDiv.className = "task_liste";
+
+    createDiv.innerHTML = txarea.value;
+
+    document.getElementById("task_container").appendChild(createDiv);
+
+    if(txarea.value =="")
+    {
+        return;
+    }
+
+    txarea.value= "";
 }
